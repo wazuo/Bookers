4497f48e4e6cdeea178d5cf90b8fe25620d8e02f
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-11 05:52:58 +0000

    [Add]3-9 Viewファイルの追加まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
 end
