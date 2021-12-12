4fa36a67b5595517ddf49c77f574518c7c865ed8
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-12 13:27:51 +0000

    [Update]3-9 book新規投録昨日作成中 コントローラーprivateの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,18 @@ class BooksController < ApplicationController
 
   def show
   end
+  
+  def new
+    @book = book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(books)
+  end  
 
   def edit
   end
 end
+
