6c2295f28b47dc33e4886f0c1c4fef818a861dd4
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-13 14:17:01 +0000

    [Add]3-9バリデーション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,12 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:'createに成功しました'
+    else
+      render :index
+    end
   end
 
   def edit
@@ -22,7 +25,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book),notice:'updateに成功しました'
   end
   def destroy
     book = Book.find(params[:id])
